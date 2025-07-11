e5cb07372ab6bdbae13201f3ca06c8b753a63771
fonnymunkey <fonnymunkey@gmail.com>
2025-07-11 00:54:46 -0400

    1.0.1
    Me when refmaps decide to not generate so i have to version bump

--- src/main/java/fermiummixins/FermiumMixins.java
+++ src/main/java/fermiummixins/FermiumMixins.java
@@ -13,7 +13,7 @@ import fermiummixins.proxy.CommonProxy;
 public class FermiumMixins {
     
     public static final String MODID = "fermiummixins";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "FermiumMixins";
     public static final Logger LOGGER = LogManager.getLogger();
 
